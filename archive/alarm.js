47187357b97ca04d70265cdbad6eacabe3226498
BlazingSky <DeathStrikeV@hotmail.com>
2020-12-23 17:12:49 +0000

    Cleanup
    Signed-off-by: BlazingSky <DeathStrikeV@hotmail.com>

--- archive/alarm.js
+++ archive/alarm.js
@@ -4,6 +4,7 @@ const { exit } = require('process');
 const playlist_path = `C:\\Users\\BlazingSky\\AppData\\Roaming\\Winamp\\Plugins\\ml\\playlists\\plf8E17.m3u8`
 const player_path   = `M:\\Projects\\Development\\Alarm\\tools\\foobar2000\\foobar2000.exe`
 const svw_path      = `M:\\Projects\\Development\\Alarm\\tools\\SoundVolumeView.exe`
+const svw_target    = `Headphones`
 
 const initial_volume = 10
 const final_volume   = 50
@@ -33,7 +34,7 @@ function sleep(ms) {
 }
 
 async function alarm() {
-    await execute(`${svw_path} /SetVolume "Headphones" ${initial_volume}`)
+    await execute(`${svw_path} /SetVolume "${svw_target}" ${initial_volume}`)
     execute(`"${player_path}" "${playlist_path}"`)
     await sleep(5000)
 
@@ -41,7 +42,7 @@ async function alarm() {
         for (let x = 0, steps = 40; x <= steps; x++) {
             volume_change = (final_volume - initial_volume) * x / steps
             volume = initial_volume + volume_change
-            await execute(`${svw_path} /SetVolume "Headphones" ${volume}`)
+            await execute(`${svw_path} /SetVolume "${svw_target}" ${volume}`)
             await sleep(1000)
         }
     }
