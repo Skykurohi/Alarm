0c1797bb31e5ed3cada147fd41fb0c9d0ff2e866
BlazingSky <DeathStrikeV@hotmail.com>
2021-03-10 12:12:06 +0000

    more indent fixes
    Signed-off-by: BlazingSky <DeathStrikeV@hotmail.com>

--- archive/alarm.js
+++ archive/alarm.js
@@ -11,44 +11,44 @@ const initial_volume = 10
 const final_volume   = 50
 
 function execute(cmd, silent = true) {
-    return new Promise (resolve => {
-        cp.exec(cmd, (error, stdout, stderr) => {
-            console.log(`${cmd}`)
+  return new Promise (resolve => {
+    cp.exec(cmd, (error, stdout, stderr) => {
+      console.log(`${cmd}`)
 
-            if (!silent) {
-                if (error) {
-                    console.error(error)
-                }
-                else {
-                    console.log(`stdout: ${stdout}`)
-                    console.log(`stderr: ${stderr}`)
-                }
-            }
+      if (!silent) {
+        if (error) {
+          console.error(error)
+        }
+        else {
+          console.log(`stdout: ${stdout}`)
+          console.log(`stderr: ${stderr}`)
+        }
+      }
 
-            resolve()
-        })
+      resolve()
     })
+  })
 }
 
 function sleep(ms) {
-    return new Promise (resolve => setTimeout(resolve, ms))
+  return new Promise (resolve => setTimeout(resolve, ms))
 }
 
 async function alarm() {
-    await execute(`${svv_path} /SetVolume "${svv_target}" ${initial_volume}`)
-    execute(`"${player_path}" "${playlist_path}"`)
-    await sleep(5000)
+  await execute(`${svv_path} /SetVolume "${svv_target}" ${initial_volume}`)
+  execute(`"${player_path}" "${playlist_path}"`)
+  await sleep(5000)
 
-    if (initial_volume < final_volume) {
-        for (let x = 0, steps = 40; x <= steps; x++) {
-            volume_change = (final_volume - initial_volume) * x / steps
-            volume = initial_volume + volume_change
-            await execute(`${svv_path} /SetVolume "${svv_target}" ${volume}`)
-            await sleep(1000)
-        }
+  if (initial_volume < final_volume) {
+    for (let x = 0, steps = 40; x <= steps; x++) {
+      volume_change = (final_volume - initial_volume) * x / steps
+      volume = initial_volume + volume_change
+      await execute(`${svv_path} /SetVolume "${svv_target}" ${volume}`)
+      await sleep(1000)
     }
+  }
 
-    exit(0)
+  exit(0)
 }
 
 alarm()
--- archive/alarm_old.bat
+++ archive/alarm_old.bat
@@ -1,15 +1,15 @@
 for /l %%x in (1, 1, 100) do (
-    nircmdc sendkeypress 0xAF
+  nircmdc sendkeypress 0xAF
 )
 
 "M:\Projects\Development\Alarm\tools\nircmdc.exe" setdefaultsounddevice "Realtek Digital Output" 1
 "M:\Projects\Development\Alarm\tools\nircmdc.exe" mutesysvolume 0
 
 for /l %%x in (1, 50, 100) do (
-	echo %%x
-    set /a volume = 65535 * %%x / 100
-	"M:\Projects\Development\Alarm\tools\nircmdc.exe" setsysvolume %volume%
-	sleep 0.5
+  echo %%x
+  set /a volume = 65535 * %%x / 100
+  "M:\Projects\Development\Alarm\tools\nircmdc.exe" setsysvolume %volume%
+  sleep 0.5
 )
 
 "C:\Program Files (x86)\Winamp\winamp.exe" "C:\Users\BlazingSky\AppData\Roaming\Winamp\Plugins\ml\playlists\plf8E17.m3u8"
