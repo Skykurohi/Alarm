1fceb847e098aab34d8e4565734bb0bd6b261a56
BlazingSky <DeathStrikeV@hotmail.com>
2021-03-10 12:10:22 +0000

    Updating indents
    Signed-off-by: BlazingSky <DeathStrikeV@hotmail.com>

--- alarm.py
+++ alarm.py
@@ -12,25 +12,25 @@ initial_volume = 10
 final_volume   = 20
 
 def execute(cmd, wait=True):
-    print(cmd)
-    if wait:
-        subprocess.call(cmd, shell=True)
-    else:
-        subprocess.Popen(cmd, shell=True)
+  print(cmd)
+  if wait:
+    subprocess.call(cmd, shell=True)
+  else:
+    subprocess.Popen(cmd, shell=True)
 
 def main():
-    execute(f'{svv_path} /SetVolume "{svv_target}" {initial_volume:.1f}')
-    execute(f'"{player_path}" "{playlist_path}"', False)
+  execute(f'{svv_path} /SetVolume "{svv_target}" {initial_volume:.1f}')
+  execute(f'"{player_path}" "{playlist_path}"', False)
 
-    if initial_volume < final_volume:
-        x     = 0
-        steps = final_volume - initial_volume
-        while x <= steps:
-            volume_change = (final_volume - initial_volume) * x / steps
-            volume = initial_volume + volume_change
-            execute(f'{svv_path} /SetVolume "{svv_target}" {volume:.1f}')
-            time.sleep(4)
-            x += 1
+  if initial_volume < final_volume:
+    x     = 0
+    steps = final_volume - initial_volume
+    while x <= steps:
+      volume_change = (final_volume - initial_volume) * x / steps
+      volume = initial_volume + volume_change
+      execute(f'{svv_path} /SetVolume "{svv_target}" {volume:.1f}')
+      time.sleep(4)
+      x += 1
 
 start = time.perf_counter()
 main()
